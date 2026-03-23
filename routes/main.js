ef040c7acfbc8849d42bd08df64224145ff7ebb1
Ronin619 <eytam619@gmail.com>
2026-03-22 20:55:10 -0400

    implement get route for /products/:product/review

--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,7 @@ const faker = require("faker");
 const Product = require("../models/product");
 const Reviews = require("../models/reviews");
 const { query } = require("express");
+const reviews = require("../models/reviews");
 
 // router.get("/generate-fake-data", (req, res, next) => {
 //   for (let i = 0; i < 90; i++) {
@@ -42,6 +43,35 @@ router.get("/products/:product", (req, res, next) => {
         return res.status(404).send("Product not found");
       }
       res.send(product);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Server error");
+    });
+});
+
+router.get("/products/:product/review", (req, res, next) => {
+  const pages = req.query.page || 1;
+  const id = req.params.product;
+  const limit = 4;
+
+  Product.findById(id)
+    .populate("reviews")
+    .then((product) => {
+      if (!product) {
+        return res.status(404).send("Product not found");
+      }
+
+      const start = (pages - 1) * limit;
+      const end = pages * limit;
+
+      const paginatedReviews = product.reviews.slice(start, end);
+
+      if (paginatedReviews.length === 0) {
+        return res.status(404).send("No reviews found");
+      }
+
+      res.send(paginatedReviews);
     })
     .catch((err) => {
       console.error(err);
