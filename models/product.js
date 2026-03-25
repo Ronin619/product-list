d70d78182f9d38fcbedfb46e7f432d183992eb52
Ronin619 <eytam619@gmail.com>
2026-03-24 20:53:51 -0400

    update product schema to have required paths

--- models/product.js
+++ models/product.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema({
-  category: String,
-  name: String,
-  price: Number,
+  category: { type: String, required: true },
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
   image: String,
   reviews: [{ type: Schema.Types.ObjectId, ref: "Reviews" }],
 });
