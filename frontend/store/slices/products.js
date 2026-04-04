776a4b3b41501a5077053648ae7e9c5f18c210de
Ronin619 <eytam619@gmail.com>
2026-04-03 22:19:52 -0400

    update products slice to include total pages and current page

--- frontend/store/slices/products.js
+++ frontend/store/slices/products.js
@@ -3,29 +3,34 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const ROOT_URL = "http://localhost:8000/products";
 
-export const fetchProducts = createAsyncThunk("get/products", async () => {
-  const response = await axios.get(ROOT_URL);
-  const productSet = [];
+export const fetchProducts = createAsyncThunk(
+  "get/products",
+  async (page = 1) => {
+    const response = await axios.get(`${ROOT_URL}?page=${page}`);
+    const productSet = [];
 
-  response.data.forEach((product) => {
-    const productCard = {
-      id: product._id,
-      category: product.category,
-      price: product.price,
-      image: product.image,
-      name: product.name,
-    };
+    response.data.products.forEach((product) => {
+      const productCard = {
+        id: product._id,
+        category: product.category,
+        price: product.price,
+        image: product.image,
+        name: product.name,
+      };
 
-    productSet.push(productCard);
-  });
-  console.log(productSet);
-  return productSet;
-});
+      productSet.push(productCard);
+    });
+    console.log(productCard);
+    return productSet;
+  },
+);
 
 export const productsSlice = createSlice({
   name: "products",
   initialState: {
     products: [],
+    totalPages: 1,
+    currentPage: 1,
     status: "idle",
     error: null,
   },
@@ -37,7 +42,9 @@ export const productsSlice = createSlice({
       })
       .addCase(fetchProducts.fulfilled, (state, action) => {
         state.status = "succeeded";
-        state.top10Anime = action.payload;
+        state.products = action.payload;
+        state.totalPages = action.payload.totalPages;
+        state.currentPage = action.payload.currentPage;
       })
       .addCase(fetchProducts.rejected, (state, action) => {
         state.status = "failed";
