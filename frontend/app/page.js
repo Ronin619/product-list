88026b34d4a6226ab8b3753d3d0b973d9cd0f59f
Ronin619 <eytam619@gmail.com>
2026-04-03 21:36:04 -0400

    implemeent fetch products function on load

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,9 +1,18 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
 import Navbar from "./components/Navbar";
+import { fetchProducts } from "../store/slices/products";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Home() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
   return (
     <>
       <Navbar />
