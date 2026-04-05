9bf6dc3f170bbdb3241ec3e268846a9ad7266127
Ronin619 <eytam619@gmail.com>
2026-04-05 16:54:57 -0400

    update grid styling

--- frontend/app/components/ProductGrid.js
+++ frontend/app/components/ProductGrid.js
@@ -3,30 +3,32 @@ import Image from "next/image";
 
 export default function ProductGrid() {
   const products = useSelector((state) => state.products.products);
-  console.log(products);
+
   const card = products.map((product) => {
     return (
-      <div className="card" key={product.id}>
-        <div className="card-header">
-          <p className="card-category">{`Category: ${product.category}`}</p>
-          <h3 className="card-price">{product.price}</h3>
-        </div>
-        <div className="card-body">
-          <Image
-            src={product.image}
-            width={200}
-            height={200}
-            alt="Picture of the product"
-          ></Image>
-          <h2 className="card-name">{product.name}</h2>
+      <div className="col-md-4" key={product.id}>
+        <div className="card">
+          <div className="card-header">
+            <p className="card-category">{`Category: ${product.category}`}</p>
+            <h3 className="card-price">{product.price}</h3>
+          </div>
+          <div className="card-body">
+            <Image
+              src={product.image}
+              width={200}
+              height={200}
+              alt="Picture of the product"
+            ></Image>
+            <h2 className="card-name">{product.name}</h2>
+          </div>
         </div>
       </div>
     );
   });
 
   return (
-    <div className="container">
-      <div className="row row-cols-1 row-cols-md-3 g-3">{card}</div>
+    <div className="container" style={{ maxWidth: "900px" }}>
+      <div className="row row-cols-1 row-cols-md-3 g-4">{card}</div>
     </div>
   );
 }
