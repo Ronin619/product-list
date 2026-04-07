12219e37fa6d169bba4ca17ced72f590a23625dc
Ronin619 <eytam619@gmail.com>
2026-04-06 21:40:33 -0400

    update add name to query param in async thunk

--- frontend/store/slices/products.js
+++ frontend/store/slices/products.js
@@ -5,8 +5,8 @@ const ROOT_URL = "http://localhost:8000/products";
 
 export const fetchProducts = createAsyncThunk(
   "get/products",
-  async (page = 1) => {
-    const response = await axios.get(`${ROOT_URL}?page=${page}`);
+  async (page = 1, name = "") => {
+    const response = await axios.get(`${ROOT_URL}?page=${page}&name=${name}`);
 
     const productSet = response.data.products.map((product) => ({
       id: product._id,
