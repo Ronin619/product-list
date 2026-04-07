39e45dccc94dc592d422435b01b9572c4811a374
Ronin619 <eytam619@gmail.com>
2026-04-06 22:46:22 -0400

    implement async thunk fetchCategories and categories to product slice

--- frontend/store/slices/products.js
+++ frontend/store/slices/products.js
@@ -5,8 +5,10 @@ const ROOT_URL = "http://localhost:8000/products";
 
 export const fetchProducts = createAsyncThunk(
   "get/products",
-  async (page = 1, name = "") => {
-    const response = await axios.get(`${ROOT_URL}?page=${page}&name=${name}`);
+  async (page = 1, name = "", category = "") => {
+    const response = await axios.get(
+      `${ROOT_URL}?page=${page}&name=${name}&category=${category}`,
+    );
 
     const productSet = response.data.products.map((product) => ({
       id: product._id,
@@ -23,12 +25,18 @@ export const fetchProducts = createAsyncThunk(
   },
 );
 
+export const fetchCategories = createAsyncThunk("get/categories", async () => {
+  const response = await axios.get("http://localhost:8000/categories");
+  return response.data;
+});
+
 export const productsSlice = createSlice({
   name: "products",
   initialState: {
     products: [],
     totalPages: 1,
     currentPage: 1,
+    categories: [],
     status: "idle",
     error: null,
   },
@@ -45,6 +53,13 @@ export const productsSlice = createSlice({
         state.currentPage = action.payload.currentPage;
       })
       .addCase(fetchProducts.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      })
+      .addCase(fetchCategories.fulfilled, (state, action) => {
+        state.categories = action.payload;
+      })
+      .addCase(fetchCategories.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.error.message;
       });
