dd2a05b533d67044db48cb67ce733b3ad5549ea0
Ronin619 <eytam619@gmail.com>
2026-04-13 21:25:48 -0400

    implement log in page

---
+++ frontend/app/components/LoginPage.js
@@ -0,0 +1,10 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Please log in</h1>
+      <a href="http://localhost:8000/auth/google">
+        <button>Login with Google</button>
+      </a>
+    </div>
+  );
+}
